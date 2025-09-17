b0be5216762bc0a3a36355205977a2013871735e
SrVagem <joaocalhau7@gmail.com>
2025-09-16 21:00:55 -0300

    merge remoto

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      // Não quebre o build por causa de lint no Vercel
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // (opcional) Não quebre o build por erros de TS no Vercel
-      ignoreBuildErrors: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+module.exports = nextConfig;
